a9331517f05a180e11fd8d626c2f427e40b884c3
Baraka Larbi <laridev@gmail.com>
2019-01-26 14:53:33 +0100

    commonJs Exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import nav from './nav';
-import { top , bottom} from './footer'
+import { top , bottom} from './footer';
+
+//webpack doesnot like common js 
+//so use esm for it
+import makeButton from './button';
+
+ 
 console.log(nav);
-console.log(top, bottom);
+console.log(top, bottom , makeButton('submit Button'));
